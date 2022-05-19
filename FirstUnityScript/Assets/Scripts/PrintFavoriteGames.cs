2bdbee6957e69395f88f9211bd9704c4e038393e
Sk Solaiman Abdullah <dominictorreto1616@gmail.com>
2022-05-19 20:23:54 +0600

    More unity works added successfully

---
+++ FirstUnityScript/Assets/Scripts/PrintFavoriteGames.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintFavoriteGames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("My three favorite games are: PUBG,COD & Ludo");
+    }
+}
